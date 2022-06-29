74a1f64257cf51eca49c3ea7fb079f27f27d0611
shuvam77 <shuvam77@gmail.com>
2022-06-29 23:21:40 +0200

    orders model completed

--- orders/models.py
+++ orders/models.py
@@ -18,3 +18,20 @@ class Order(models.Model):
     total_paid = models.DecimalField(max_digits=5, decimal_places=2)
     order_key = models.CharField(max_length=200)
     billing_status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.created)
+
+
+class OderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
